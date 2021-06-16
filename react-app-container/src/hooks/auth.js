8ca3d0447c944f18396af84039168143892b3d5c
lpaulovt <silvavitorlopes711@gmail.com>
2021-06-16 16:16:51 -0300

    :sparkles: Creating dockerfiles

--- react-app-container/src/hooks/auth.js
+++ react-app-container/src/hooks/auth.js
@@ -1,45 +1,47 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
-import { listenEvent } from '@d3/utils'
-
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { listenEvent } from "@d3/utils";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const [logged, setLogged] = useState(() => {
-    const isLogged = localStorage.getItem('@d3-mfe-zag:logged');
+    const isLogged = localStorage.getItem("@d3-mfe-zag:logged");
 
     return !!isLogged;
   });
 
   const signIn = (email, password) => {
     setIsLoading(true);
-   /* if (email === 'paulo@d3.do' && password === '123') {
-      localStorage.setItem('@d3-mfe-zag:logged', 'true');
+    if (email === "paulo@d3.do" && password === "123") {
+      localStorage.setItem("@d3-mfe-zag:logged", "true");
       setLogged(true);
+      setIsLoading(false);
     } else {
-      alert('Senha ou usuário inválidos!');
-    }*/
-  }
+      alert("Senha ou usuário inválidos!");
+    }
+  };
 
   const signOut = () => {
-    localStorage.removeItem('@d3-mfe-zag:logged');
+    localStorage.removeItem("@d3-mfe-zag:logged");
     setLogged(false);
-  }
+  };
 
   useEffect(() => {
-    listenEvent('@d3/react-menu/signout', () => {
+    listenEvent("@d3/react-menu/signout", () => {
       signOut();
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ logged, signIn, signOut, isLoading: isLoading }}>
+    <AuthContext.Provider
+      value={{ logged, signIn, signOut, isLoading: isLoading }}
+    >
       {children}
     </AuthContext.Provider>
   );
-}
+};
 
 function useAuth() {
   const context = useContext(AuthContext);
